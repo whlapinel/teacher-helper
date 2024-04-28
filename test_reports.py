003a787cff62b8169fb48397696a593407e3b7f4
whlapinel <140720302+whlapinel@users.noreply.github.com>
2024-04-28 00:37:50 -0400

    added analysis report csv parsing and finding problem questions. Just need to fix navigation to download report.

--- test_reports.py
+++ test_reports.py
@@ -1,4 +1,4 @@
-
+import pandas as pd
 import find_csv
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
@@ -10,15 +10,25 @@ class CanvasReport:
 
 
 def get_report(class_number, test_id, driver, wait):
-    return "hello"
+    download_report(class_number, test_id, driver, wait)
+    file_path = find_csv.get_latest_csv()
+    report = load_report(file_path)
+    return report
 
 
 def download_report(class_number, test_id, driver, wait):
+    # fixme: this url is correct but not if you go to the page manually
+    # must mouse over the following element a['data-fullname'=test_name] and then click on 
+    # a.openBigDialog with href below to get the modal for the test_id. (element text is "Reports")
     reports_url = f'https://app.masteryconnect.com/classrooms/{class_number}/benchmarks/{test_id}/modal?tab=reports'
     driver.get(reports_url)
     download_btn = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'button#ia-csv-button')))
     download_btn.click()
 
 
-def load_report():
-    file_path = find_csv.get_latest_csv()
+def load_report(file_path):
+    df = pd.read_csv(file_path)
+    percentages_dict = df.iloc[7].to_dict()
+    trimmed_dict = {k: v for k, v in percentages_dict.items() if not (('Unnamed' in k) or ('question_number' in k))}
+    less_than_60 = {k: v for k, v in trimmed_dict.items() if int(v) < 60}
+    return list(less_than_60.keys())
